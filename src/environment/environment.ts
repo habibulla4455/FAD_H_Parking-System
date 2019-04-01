80289c5a6c0598f3e6289e55819f4012367a3723
habibulla4455 <habibulla4455@gmail.com>
2019-04-01 20:43:49 +0530

    first-commit

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: false,
     firebase: {
-        apiKey: "AIzaSyDmhfetUxd8YmVNMq0UGGuV7Mu1teEkytI",
-        authDomain: "smart-parking-systems-9371.firebaseapp.com",
-        databaseURL: "https://smart-parking-systems-9371.firebaseio.com",
-        projectId: "smart-parking-systems-9371",
-        storageBucket: "smart-parking-systems-9371.appspot.com",
-        messagingSenderId: "390138654761"
+        apiKey: "AIzaSyDSs2ptd2Dzi9C5XxNGffXy-ushVBabyOM",
+        authDomain: "habib-e7999.firebaseapp.com",
+        databaseURL: "https://habib-e7999.firebaseio.com",
+        projectId: "habib-e7999",
+        storageBucket: "habib-e7999.appspot.com",
+        messagingSenderId: "593669101940"
     }
 }
